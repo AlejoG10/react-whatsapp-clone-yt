f34d5d4822d255a31d1b112c6fd4cc8bbfce8c6f
AlejoG10-uniandes <alejogonza99@hotmail.com>
2022-11-09 21:07:51 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        "whatsapp-bp": "755px",
+      },
+    },
+  },
+  plugins: [],
+};
